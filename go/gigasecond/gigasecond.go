99958ae64a58b483026c64c648670184f84d7a61
Alex Strick van Linschoten <alexstrick@mm.st>
2018-06-24 12:28:27 +0100

    add exponent function to gigasecond.go (still failing tests)

--- go/gigasecond/gigasecond.go
+++ go/gigasecond/gigasecond.go
@@ -7,6 +7,16 @@ import "time"
 // AddGigasecond is the main function of the package.
 func AddGigasecond(t time.Time) time.Time {
 	start := t
-	gigavalue := start.Add(time.Second * (10 ^ 9))
+	gigavalue := start.Add(time.Second * (exponent(10, 9)))
 	return gigavalue
+}
+
+// exponent is a function to calculate the exponent of two integers.
+func exponent(number, power int) int {
+	exp := number
+	for i := 0; i < power-1; i++ {
+		fmt.Println(i)
+		exp = exp * number
+	}
+	return exp
 }
