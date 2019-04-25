dff9fc9d90311fbb7261aad7aa8ebdb35658ea91
Alex Strick van Linschoten <stricksubscriptions@fastmail.fm>
2019-04-25 18:26:09 +0100

    update solution

--- ruby/two-fer/two_fer.rb
+++ ruby/two-fer/two_fer.rb
@@ -1,6 +1,5 @@
 class TwoFer
-  def self.two_fer(name = "")
-    return "One for you, one for me." if name == ""
+  def self.two_fer(name = "you")
     return "One for #{name}, one for me."
   end
 end
