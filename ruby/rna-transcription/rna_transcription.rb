e58dfd06a9e64f23d6b8dfa8300ed1709902b119
Alex Strick van Linschoten <stricksubscriptions@fastmail.fm>
2019-04-28 09:09:06 +0100

    refactor RNA code as per mentor's suggestions

--- ruby/rna-transcription/rna_transcription.rb
+++ ruby/rna-transcription/rna_transcription.rb
@@ -1,13 +1,5 @@
 class Complement
-  DNA_TO_RNA = {
-    "A" => "U",
-    "T" => "A",
-    "C" => "G",
-    "G" => "C"
-  }
   def self.of_dna(string)
-    string.chars.map do |char|
-      DNA_TO_RNA[char]
-    end.join("")
+    string.tr('ATCG', 'UAGC')
   end
 end
