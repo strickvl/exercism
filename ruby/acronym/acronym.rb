d5b1634f1ff6ba1149f5879d0ed70d0b685d9405
Alex Strick van Linschoten <stricksubscriptions@fastmail.fm>
2019-05-08 23:38:32 +0100

    refactor code and tests

--- ruby/acronym/acronym.rb
+++ ruby/acronym/acronym.rb
@@ -1,10 +1,5 @@
 class Acronym
   def self.abbreviate(string)
-    clean_string = string.gsub(" - ", "-")
-    abbrev = string.chars.first.upcase
-    clean_string.chars.each_with_index do |char, idx|
-      abbrev << clean_string[idx+1].upcase if char == " " || char == "-"
-    end
-    abbrev
+    string.scan(/\b[a-zA-Z]/).join("").upcase
   end
 end
