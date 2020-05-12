435275adb7dd472b54da8e63efd78d33d245c552
Alex Strick van Linschoten <stricksubscriptions@fastmail.fm>
2020-05-12 14:56:35 +0500

    complete solution

--- ruby/leap/leap.rb
+++ ruby/leap/leap.rb
@@ -1,8 +1,5 @@
 class Year
   def self.leap?(year)
-    return true if year % 400 == 0
-    return false if year % 100 == 0 && year % 4 == 0
-    return true if year % 4 == 0
-    false
+    year % 400 == 0 || !(year % 100 == 0 && year % 4 == 0)
   end
 end
--- ruby/space-age/space_age.rb
+++ ruby/space-age/space_age.rb
@@ -1,2 +1,37 @@
 class SpaceAge
+  def initialize(seconds)
+    @age_in_seconds = seconds
+  end
+
+  def on_earth
+    (@age_in_seconds.to_f / 31557600).round(2)
+  end
+
+  def on_mercury
+    (@age_in_seconds.to_f / (31557600 * 0.2408467)).round(2)
+  end
+
+  def on_venus
+    (@age_in_seconds.to_f / (31557600 * 0.61519726)).round(2)
+  end
+
+  def on_mars
+    (@age_in_seconds.to_f / (31557600 * 1.8808158)).round(2)
+  end
+
+  def on_jupiter
+    (@age_in_seconds.to_f / (31557600 * 11.862615)).round(2)
+  end
+
+  def on_saturn
+    (@age_in_seconds.to_f / (31557600 * 29.447498)).round(2)
+  end
+
+  def on_uranus
+    (@age_in_seconds.to_f / (31557600 * 84.016846)).round(2)
+  end
+
+  def on_neptune
+    (@age_in_seconds.to_f / (31557600 * 164.79132)).round(2)
+  end
 end
