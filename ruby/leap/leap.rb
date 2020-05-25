f9cab7ccb1a0de9c2e62844996c643cd379c8d27
Alex Strick van Linschoten <stricksubscriptions@fastmail.fm>
2020-05-25 13:43:10 +0500

    refactor code

--- ruby/leap/leap.rb
+++ ruby/leap/leap.rb
@@ -1,5 +1,5 @@
 class Year
   def self.leap?(year)
-    (year % 4).zero? && (year % 100).nonzero? || (year % 400).zero?
+    ((year % 4).zero? && (year % 100).nonzero?) || (year % 400).zero?
   end
 end
