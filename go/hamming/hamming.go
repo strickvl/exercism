cf53e064338dec543e35fe738e1dda3dfc97dec7
Alex Strick van Linschoten <stricksubscriptions@fastmail.fm>
2021-01-22 09:49:43 +0000

    refactor code
    - ran gofmt
    - returned nil where no error needed returning

--- go/hamming/hamming.go
+++ go/hamming/hamming.go
@@ -1,26 +1,25 @@
 package hamming
 
 import (
-	"strings"
 	"errors"
+	"strings"
 )
 
 func Distance(a, b string) (int, error) {
 	aSlice := strings.Split(a, "")
 	bSlice := strings.Split(b, "")
 	distance := 0
-	var err error
-	
+
 	if len(aSlice) != len(bSlice) {
-		err = errors.New("The length isn't right")
+		err := errors.New("the length isn't right")
 		return distance, err
 	}
-	
+
 	for i, _ := range aSlice {
 		if aSlice[i] != bSlice[i] {
 			distance++
 		}
 	}
 
-	return distance, err
+	return distance, nil
 }
