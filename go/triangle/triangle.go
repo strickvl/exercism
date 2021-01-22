c5510a185cd5a4cf28873dc496a01b42f3c3f1e1
Alex Strick van Linschoten <stricksubscriptions@fastmail.fm>
2021-01-22 12:10:58 +0000

    solve triangle

--- go/triangle/triangle.go
+++ go/triangle/triangle.go
@@ -1,28 +1,37 @@
-// This is a "stub" file.  It's a little start on your solution.
-// It's not a complete solution though; you have to write some code.
-
-// Package triangle should have a package comment that summarizes what it's about.
-// https://golang.org/doc/effective_go.html#commentary
 package triangle
 
+import "math"
 
-// Notice KindFromSides() returns this type. Pick a suitable data type.
-type Kind
+type Kind string
 
 const (
-    // Pick values for the following identifiers used by the test program.
-    NaT // not a triangle
-    Equ // equilateral
-    Iso // isosceles
-    Sca // scalene
+	NaT = "NaT" // not a triangle
+	Equ = "Equ" // equilateral
+	Iso = "Iso"  // isosceles
+	Sca = "Sca" // scalene
 )
 
-// KindFromSides should have a comment documenting it.
-func KindFromSides(a, b, c float64) Kind {
-	// Write some code here to pass the test suite.
-	// Then remove all the stock comments.
-	// They're here to help you get started but they only clutter a finished solution.
-	// If you leave them in, reviewers may protest!
-	var k Kind
-	return k
+func IsInvalidTriangle(a, b, c float64) bool {
+	if a <= 0 || b <= 0 || c <= 0 {
+		return true
+	} else if a + b < c || b + c < a || c + a < b {
+		return true
+	} else if math.IsNaN(a) || math.IsNaN(b) || math.IsNaN(c) || math.IsInf(a, 0) || math.IsInf(b, 0) || math.IsInf(c, 0) {
+		return true
+	}
+	return false
+}
+
+// KindFromSides returns a triangle's type given the length of its sides
+func KindFromSides(a, b, c float64) (k Kind) {
+	if IsInvalidTriangle(a, b, c) {
+		k = Kind(NaT)
+	} else if a == b && b == c && c == a {
+		k = Kind(Equ)
+	} else if a == b || b == c || c == a {
+		k = Kind(Iso)
+	} else if a != b && b != c && c != a {
+		k = Kind(Sca)
+	}
+	return
 }
