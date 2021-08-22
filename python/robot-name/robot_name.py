b0ca03bbb6b47e0990d0d0626e65af06d2c5f184
Alex Strick van Linschoten <strickvl@users.noreply.github.com>
2021-08-22 15:31:58 +0200

    update correct solution

--- python/robot-name/robot_name.py
+++ python/robot-name/robot_name.py
@@ -1,13 +1,24 @@
-def generate_new_name():
-    return "Alex"
+import random
+
+LETTERS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+NUMBERS = "0123456789"
 
 class Robot:
     assigned_names = []
 
     def __init__(self):
-        self.name = generate_new_name()
-        Robot.assigned_names.append("Alex")
+        self.name = self.generate_new_name()
+        Robot.assigned_names.append(self.name)
 
+    def generate_new_name(self):
+        letters = "".join(random.choices(LETTERS, k=2))
+        numbers = "".join(random.choices(NUMBERS, k=3))
+        new_name = letters + numbers
+        while new_name in Robot.assigned_names:
+            new_name = self.generate_new_name()
 
-robot = Robot()
-print(robot.name)
+        return new_name
+
+    def reset(self):
+        self.name = self.generate_new_name()
+        Robot.assigned_names.append(self.name)
