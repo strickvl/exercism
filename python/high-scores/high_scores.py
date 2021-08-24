ce818127336652e07a9315126a4aa3c00928b7c2
Alex Strick van Linschoten <strickvl@users.noreply.github.com>
2021-08-24 21:11:06 +0200

    refactored code (and added type annotations)

--- python/high-scores/high_scores.py
+++ python/high-scores/high_scores.py
@@ -1,10 +1,10 @@
-def latest(scores):
+def latest(scores: list) -> int:
     return scores[-1]
 
 
-def personal_best(scores):
+def personal_best(scores: list) -> int:
     return max(scores)
 
 
-def personal_top_three(scores):
-    return sorted(scores, reverse=True)[0:3]
+def personal_top_three(scores: list) -> list:
+    return sorted(scores, reverse=True)[:3]
