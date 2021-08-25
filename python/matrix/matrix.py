b5ffd2b09556e86da22c5ec5bba38238584b77d5
Alex Strick van Linschoten <strickvl@users.noreply.github.com>
2021-08-25 21:42:01 +0200

    refactor with type annotations

--- python/matrix/matrix.py
+++ python/matrix/matrix.py
@@ -1,12 +1,9 @@
 class Matrix:
-    def __init__(self, matrix_string):
+    def __init__(self, matrix_string: str):
         self.matrix = [[int(num) for num in row.split()] for row in matrix_string.splitlines()]
 
-    def row(self, index):
+    def row(self, index: int) -> list:
         return self.matrix[index - 1]
 
-    def column(self, index):
-        col = []
-        for row in self.matrix:
-            col.append(row[index - 1])
-        return col
+    def column(self, index: int) -> list:
+        return [row[index - 1] for row in self.matrix]
