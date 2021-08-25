3a34d3ff0b54dcda0dcee022f91562b172671156
Alex Strick van Linschoten <strickvl@users.noreply.github.com>
2021-08-25 20:52:13 +0200

    remove inconsistent quotation marks

--- python/matrix/matrix.py
+++ python/matrix/matrix.py
@@ -1,6 +1,6 @@
 class Matrix:
     def __init__(self, matrix_string):
-        self.matrix = [[int(num) for num in row.split(" ")] for row in matrix_string.split('\n')]
+        self.matrix = [[int(num) for num in row.split(" ")] for row in matrix_string.split("\n")]
 
     def row(self, index):
         return self.matrix[index - 1]
