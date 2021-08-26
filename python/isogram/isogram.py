0bac65acc5e36ce3b216c5f47b8a5e9ded7e6fd0
Alex Strick van Linschoten <strickvl@users.noreply.github.com>
2021-08-26 19:12:41 +0200

    solve isogram

---
+++ python/isogram/isogram.py
@@ -0,0 +1,12 @@
+import re
+
+def is_isogram(string: str) -> bool:
+    chars = re.sub(r'[^a-zA-Z]', '', string).lower()
+    char_counts = {}
+    for char in chars:
+        if char in char_counts.keys():
+            return False
+        else:
+            char_counts[char] = True
+
+    return True
