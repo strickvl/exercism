51641a3968268e89c2371baba6be6035302f95ce
Alex Strick van Linschoten <strickvl@users.noreply.github.com>
2021-08-26 19:55:09 +0200

    refactor

--- python/isogram/isogram.py
+++ python/isogram/isogram.py
@@ -4,7 +4,7 @@ def is_isogram(string: str) -> bool:
     chars = re.sub(r'[^a-zA-Z]', '', string).lower()
     char_counts = {}
     for char in chars:
-        if char in char_counts.keys():
+        if char in char_counts:
             return False
         else:
             char_counts[char] = True
