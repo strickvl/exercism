4a56d975dc30aa901e0489b9c01a77e278449386
Alex Strick van Linschoten <strickvl@users.noreply.github.com>
2021-08-26 20:26:06 +0200

    add solution

---
+++ python/grains/grains.py
@@ -0,0 +1,13 @@
+def square(number):
+    if number < 1:
+        raise ValueError("number must be one or greater")
+    elif number > 64:
+        raise ValueError("number can't be greater than 64")
+
+    grains_val = 1
+    for num in range(1, number):
+        grains_val = grains_val * 2
+    return grains_val
+
+def total():
+    return (square(64) * 2) - 1
