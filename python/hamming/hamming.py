ca030e1f5c1047693419465ff5e3c8bf319c5a21
Alex Strick van Linschoten <strickvl@users.noreply.github.com>
2021-08-26 18:40:59 +0200

    refactor hamming to use list comprehension

--- python/hamming/hamming.py
+++ python/hamming/hamming.py
@@ -2,9 +2,6 @@ def distance(strand_a: str, strand_b: str) -> int:
     if len(strand_a) != len(strand_b):
         raise ValueError("the two strings must be of equal length")
 
-    hamming_val = 0
-    for idx in range(len(strand_a)):
-        if (strand_a[idx] != strand_b[idx]):
-            hamming_val += 1
+    inconsistent_elements = [1 for val in range(len(strand_a)) if strand_a[val] != strand_b[val]]
 
-    return hamming_val
+    return sum(inconsistent_elements)
